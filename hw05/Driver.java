7060432eda03b8df0fc3a09d9df27e8dd8e05e70
Kaden Turner <kadenturner19@gmail.com>
2025-09-24 08:14:56 -0500

    Updated the hw05 folder as well as made the attributes in the Movie class private

---
+++ hw05/Driver.java
@@ -0,0 +1,34 @@
+/**
+ * Driver class used to test the MovieContainer class
+ *
+ * @author  Kaden Turner
+ * @version 9/17/2025
+ *
+ * <pre>
+ * This is the beginning of hw04
+ * </pre>
+*/
+public class Driver
+{
+   public static void main(String [] args)
+   {
+   MovieContainer mc = new MovieContainer();
+   mc.display();
+   
+   mc.insert("Kaden","Action",2005);
+   mc.insert("Kaden1","Action",2006);
+   mc.insert("Kaden2","Action",2007);
+   mc.insert("hello", "Suspense",2008);
+   mc.display();
+   mc.searchByYear(2005);
+   mc.searchByTitle("kaden");
+   mc.searchByTitle("hello");
+   mc.searchByGenre("Action");
+   mc.searchByGenre("Suspense");
+   mc.display();
+   mc.remove(3);
+   mc.display();
+   mc.save();
+   mc.size();
+   }
+}
---
+++ hw05/MovieContainer.java
@@ -0,0 +1,173 @@
+/**
+ * Personal Movie Inventory System.
+ *
+ * @author  Kaden Turner
+ * @version 9/17/2025
+ *
+ * <pre>
+ * This is the starter code for the parallel array version of the movie
+ * inventory system.
+ * </pre>
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class MovieContainer
+{
+	private static final String DATAFILE= "../data/movielist.txt";
+   private int numMovies = 0;                         //Created a new variable to hold the number of movies
+   private Movie[] movies = new Movie[10000];     //Created a new Movie array with 10000 slots
+   
+	public static final int MAXMOVIES = 10000;
+
+	/**
+	 * Allow user to enter a new movie.
+	 *
+	 * @param t new title of the movie added
+	 * @param g new genre of the movie added
+    * @param y new year of the movie added
+	 * @return the new movie count
+	*/
+	public int insert(String t, String g, int y)
+	{
+      movies[numMovies] = new Movie(t, g, y);
+      
+      numMovies++;
+		return numMovies;
+	}
+   
+	/**
+	 * Displays all movie information.
+	*/
+	public void display()
+	{
+		System.out.println("------------------------------------------------");
+		System.out.printf("%-30s %-20s %s\n","TITLE","GENRE","YEAR");
+      
+		for (int i=0; i<numMovies; i++)
+      {
+			System.out.print(movies[i]);
+      }
+	}
+   
+   /**
+	 * prompt the user to enter a year and display all movies that match the given year.
+    *
+    * @param n number of elements in the array
+	*/
+   public void searchByYear(int n)
+   {    
+      System.out.println("------------------------------------------------");
+      System.out.printf("%-30s %-20s %s\n","TITLE","GENRE","YEAR");     //Formatted Strings copied from the main method to preserve aesthetic
+      
+      for(int i = 0;i < numMovies;i++)
+      {
+         if(movies[i].getYear() == n)
+            System.out.printf("%-30s %-20s %4d\n", movies[i].getTitle(), movies[i].getGenre(), movies[i].getYear());
+      }
+   }
+   
+  /**
+   * prompt the user to enter a title and display all movies that match the given title.
+   *
+   * @param t Title to search for
+  */
+  public void searchByTitle(String t)
+  {      
+      t = t.toLowerCase();     //convert user input to lower case
+    
+      System.out.println("------------------------------------------------");
+      System.out.printf("%-30s %-20s %-5s %s\n","TITLE","GENRE","YEAR","ID");     //formatted Strings copied from the main method to preserve aesthetic
+                                                                                  //added the ID column for the deleteMovie method
+    
+      for(int i = 0;i < numMovies;i++)
+	   {
+		   if(movies[i].getTitle().toLowerCase().contains(t))  //.contain will handle the partial matching if there is any
+		   {
+			   System.out.printf("%-30s %-20s %-5d %4d\n",movies[i].getTitle(),movies[i].getGenre(),movies[i].getYear(),i);    //formatted string copied from the other methods
+                                                                                            //added the ID number
+		   }
+	   }      
+   }
+   
+   /**
+    * prompt the user to enter a genre and display all movies within that genre
+    *
+    * @param g selected genre to search for
+   */
+   public void searchByGenre(String g)
+   {  
+      g = g.toLowerCase();     //making the search case insensitive
+      
+      System.out.println("------------------------------------------------");
+      System.out.printf("%-30s %-20s %s\n","TITLE","GENRE","YEAR");     //formatted Strings copied from the main method
+      
+      for(int i = 0;i < numMovies;i++)
+      {
+         if(movies[i].getGenre().toLowerCase().equals(g))
+         {
+            System.out.print(movies[i]);      //formatted Strings copied from the main mehtod
+         }
+      } 
+   }
+   
+   /**
+    * prompt the user to enter a title to be deleted
+    *
+    * @param index index of the selected movie to delete
+    * @return the updated amount of movies in the array
+   */
+   public int remove(int index)
+   {  
+      if(index < 0 || index >= numMovies)    
+      {
+         System.out.println("Invalid ID");      //if the id is out of bounds or not valid then return nothing
+         return numMovies;
+      } 
+      
+      for(int i = index;i < numMovies - 1;i++)
+      {
+         movies[i] = movies[i+1];
+      }
+      
+      movies[numMovies - 1] = null;     //Must clear the remaining entries
+      
+      numMovies--;
+      
+      return numMovies;    //returns the numberOfEntries -1 if id is valid
+      
+   }
+   
+   /**
+    * Displays the size of the movie container
+   */
+   public void size()
+   {
+      System.out.println(numMovies);
+   }
+
+   /**
+    * Saves all the movies to the data file thats selected
+   */
+   public void save()
+   {
+      try
+      {
+         PrintStream outFile = new PrintStream(DATAFILE);      //declaring new PrintStream to print to the data file
+         
+         for(int i = 0;i < numMovies;i++)
+         {
+            outFile.println(movies[i].getTitle());
+            outFile.println(movies[i].getGenre());      //loops through each entry and updates them to the file
+            outFile.println(movies[i].getYear());
+         }
+         
+         outFile.close();     //closes the PrintStream because I wasn't born in a barn :)
+      }
+      catch(IOException e)    //catches the exception so the program doesn't break
+      {
+         System.out.println("Couldn't save data to the selected file.");
+      }
+   }
+}
