3359d36dc2a9dc43129acf6196010bea9b1534ed
Kaden Turner <kadenturner19@gmail.com>
2025-09-28 17:43:17 -0500

    Almost fully functioning program

--- hw05/Driver.java
+++ hw05/Driver.java
@@ -12,23 +12,7 @@ public class Driver
 {     
    public static void main(String [] args)
    {
-   MovieContainer mc = new MovieContainer("../data/movielist.txt");
-   mc.display();
-   
-   mc.insert("Kaden","Action",2005);
-   mc.insert("Kaden1","Action",2006);
-   mc.insert("Kaden2","Action",2100);
-   mc.insert("hello", "Suspense",2008);
-   mc.display();
-   mc.searchByYear(2100);
-   mc.searchByTitle("kaden");
-   mc.searchByTitle("hello");
-   mc.searchByGenre("Action");
-   mc.searchByGenre("Suspense");
-   mc.display();
-   mc.remove(3);
-   mc.display();
-   mc.save();
-   mc.size();
+      MovieUI mui = new MovieUI("../data/movielist.txt");
+      mui.run();
    }
 }
--- hw05/MovieUI.java
+++ hw05/MovieUI.java
@@ -39,17 +39,17 @@ public class  MovieUI
 			choice = getMenuChoice();
          
 			if (choice==1)
-				numberOfEntries = enterMovie(movies,numberOfEntries);
-			else if (choice==3)
-				displayAll(movies,numberOfEntries);
+				enterMovie();
          else if (choice==6)
-            	searchByYear(movies,numberOfEntries);
+            searchByYear();
 			else if (choice==5)
-				searchByTitle(movies, numberOfEntries);
+				searchByTitle();
          else if (choice==4)
-            searchByGenre(movies,numberOfEntries);
+            searchByGenre();
          else if (choice==2)
-            numberOfEntries = deleteMovie(movies, numberOfEntries);           
+            deleteMovie();
+         else if (choice==3)
+            mc.display();           
 		} while (choice!=0);
       
 		System.out.println("\nTHE END");
@@ -58,25 +58,18 @@ public class  MovieUI
    
 	/**
 	 * Allow user to enter a new movie.
-	 *
-	 * @param movie Array of Movie objects
-	 * @param n the actual number of movies currently in the array
-	 * @return the new movie count
 	*/
-	public static int enterMovie(Movie[] movie, int n)
+	public void enterMovie()
 	{
-		Scanner kb= new Scanner(System.in);
-		System.out.print("Enter movie title : ");
+		System.out.println("Enter movie title : ");
 		String title = kb.nextLine();
-		System.out.print("Enter movie genre: ");
+		System.out.println("Enter movie genre: ");
 		String genre = kb.nextLine();
-		System.out.print("Enter year of movie: ");
+		System.out.println("Enter year of movie: ");
 		int year = kb.nextInt();
 		kb.nextLine();
       
-      movie[n] = new Movie(title, genre, year);
-
-		return n+1;
+      mc.insert(title, genre, year);
 	}
    
 	/**
@@ -108,109 +101,48 @@ public class  MovieUI
 	}
    
    /**
-	 * prompt the user to enter a year and display all movies that match the given year.
-    *
-	 * @param movie Array of Movie Objects
-    * @param n number of elements in the array
+	 * Search movies by number of years
 	*/
-   public void searchByYear(Movie[] movie, int n)
+   public void searchByYear()
    {
-      Scanner scan = new Scanner(System.in);    //Declaring a new scanner for this method
+      int year;
       System.out.println("Enter the year you want to search for: ");
-      int search = scan.nextInt();     //Storing user input for later search
-      
-      System.out.println("------------------------------------------------");
-      System.out.printf("%-30s %-20s %s\n","TITLE","GENRE","YEAR");     //Formatted Strings copied from the main method to preserve aesthetic
-      
-      for(int i = 0;i < n;i++)
-      {
-         if(movie[i].getYear() == search)
-            System.out.printf("%-30s %-20s %4d\n", movie[i].getTitle(), movie[i].getGenre(), movie[i].getYear());
-      }
+      year = kb.nextInt();     //Storing user input for later search
+      kb.nextLine();
+      mc.searchByYear(year);
    }
    
   /**
-   * prompt the user to enter a title and display all movies that match the given title.
-   *
-	* @param movie Array of Movie Objects
-   * @param n number of elements in the array
+   * Search movies by name of title
   */
-  public void searchByTitle(Movie[] movie, int n)
+  public void searchByTitle()
   {
-      Scanner scan = new Scanner(System.in);    //declare new Scanner
       System.out.println("Enter title to search for: ");
-      String searchTitle = scan.nextLine();
-      
-      searchTitle = searchTitle.toLowerCase();     //convert user input to lower case
-    
-      System.out.println("------------------------------------------------");
-      System.out.printf("%-30s %-20s %-5s %s\n","TITLE","GENRE","YEAR","ID");     //formatted Strings copied from the main method to preserve aesthetic
-                                                                                  //added the ID column for the deleteMovie method
-    
-      for(int i = 0;i < n;i++)
-	   {
-		   if(movie[i].getTitle().toLowerCase().contains(searchTitle))  //.contain will handle the partial matching if there is any
-		   {
-			   System.out.printf("%-30s %-20s %-5d %4d\n",movie[i].getTitle(),movie[i].getGenre(),movie[i].getYear(),i);    //formatted string copied from the other methods
-                                                                                            //added the ID number
-		   }
-	   }      
+      String searchTitle = kb.nextLine();
+      mc.searchByTitle(searchTitle);    
    }
    
    /**
-    * prompt the user to enter a genre and display all movies within that genre
-    *
-    * @param movie Array of Movie Objects
-    * @param n number of elements in the array
+    * Search movies by name of genre
    */
-   public void searchByGenre(Movie[] movie, int n)
+   public void searchByGenre()
    {
-      Scanner scan = new Scanner(System.in);    //declare new scanner
       System.out.println("Enter genre to search for: ");
-      String searchGenre = scan.nextLine();
-      
-      searchGenre = searchGenre.toLowerCase();     //making the search case insensitive
-      
-      System.out.println("------------------------------------------------");
-      System.out.printf("%-30s %-20s %s\n","TITLE","GENRE","YEAR");     //formatted Strings copied from the main method
-      
-      for(int i = 0;i < n;i++)
-      {
-         if(movie[i].getGenre().toLowerCase().equals(searchGenre))
-         {
-            System.out.print(movie[i]);      //formatted Strings copied from the main mehtod
-         }
-      } 
+      String searchGenre = kb.nextLine();
+      mc.searchByGenre(searchGenre);
    }
    
    /**
     * prompt the user to enter a title to be deleted
-    *
-    * @param movie Array of Movie Objects
-    * @param n number of elements in the array
-    * @return the updated amount of movies in the array
    */
-   public int deleteMovie(Movie[] movie, int n)
+   public void deleteMovie()
    {
-      
-      Scanner scan = new Scanner(System.in);    //declare new Scanner 
-      searchByTitle(movie, n);     //shows available movies to delete
+      System.out.println("Enter the name of the title you want to delete: ");
+      String t = kb.nextLine();
+      mc.searchByTitle(t);     //shows available movies to delete
       
       System.out.println("Enter the ID number you would like to delete: ");
-      int id = scan.nextInt();      //id variable to use for deletion
-      
-      if(id < 0 || id > n)    
-      {
-         System.out.println("Invalid ID");      //if the id is out of bounds or not valid then return nothing
-         return n;
-      } 
-      
-      for(int i = id;i < n - 1;i++)
-      {
-         movie[i] = movie[i+1];
-      }
-      
-      return n-1;    //returns the numberOfEntries -1 if id is valid
-      
+      int id = kb.nextInt();      //id variable to use for deletion
+      mc.remove(id);
    }
 }
