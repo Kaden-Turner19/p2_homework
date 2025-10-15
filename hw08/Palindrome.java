a66bc04b5a73f8bdb582748cee335e9f2a90adaa
Kaden Turner <kadenturner19@gmail.com>
2025-10-15 09:42:57 -0500

    Completed the Lab day number 8. Fully functional palindrome method that will check if the input is a palindrome.

--- hw08/Palindrome.java
+++ hw08/Palindrome.java
@@ -1,16 +1,45 @@
+/**
+ * A class that will take in input and determine whether the input is a palindrome
+ *
+ * @author  Kaden Turner
+ * @version for P2 10/15/2025
+ *
+*/
 import java.util.Scanner;
 
 public class Palindrome
 {
 	public static void main(String [] args)
-	{
-      System.out.println(args.length + " parameter/s passed.");
-      
-      int i;
-      for(i = 0;i<args.length;i++)
+	{  
+      if(args.length != 1)
       {
-         System.out.println(args[i]);
+         System.out.println("Error: There are too many parameters passed, there should only be one.");
+      }  
+      else
+      {
+         System.out.println(isPalindrome(args[0]));
       }
 	}
+   
+   /**
+    * Will take in a String parameter and determine whether or not its a palindrome
+    *
+    * @Param word word to be checked
+   */
+   public static boolean isPalindrome(String word)
+   {
+      if(word == null || word.length() == 0)
+      {
+         return false;
+      }
+      
+      for(int i = 0;i<word.length() / 2;i++)
+      {
+         if(word.charAt(i) != word.charAt(word.length() - 1 - i))
+            return false;
+      }     
+      
+      return true;             
+   }
 }
 
