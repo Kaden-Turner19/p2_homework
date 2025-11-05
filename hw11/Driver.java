0ffc6528f4ab4c3979d8073ce17e6c361befb62a
Kaden Turner <kadenturner19@gmail.com>
2025-11-05 08:33:22 -0600

    Finished pre-lab assignment

---
+++ hw11/Driver.java
@@ -0,0 +1,67 @@
+/**
+ * Driver for testing IntList code.
+ *
+ * @author  Kaden Turner
+ * @version 11/5/2025
+ *
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Driver
+{
+	public static void main(String [] args)
+	{
+     Word w1 = new Word("hello");
+     Word w2 = new Word("hello");
+     Word w3 = new Word("Kaden");
+     
+     System.out.println(w1.equals(w2));
+     System.out.println(w1.equals(w3));
+   
+   
+    
+      //Code testing for the Homework 10
+      /*
+      Word w1 = new Word("kaden");
+      Word w2 = new Word("gfedcba");      //testing the word class to see functionality
+      
+      System.out.println(w1);    
+      System.out.println(w2);
+      
+      WordNode wn = new WordNode();
+      wn.data = "Kaden";
+      wn.next = new WordNode();           //testing for the WordNode class 
+      wn.next.data = "Turner";
+      
+      WordList wl = new WordList();
+      wl.insert("Kaden");
+      wl.insert("Turner");                //testing for the WordList class
+      wl.display();
+      */
+      /*
+      try
+      {
+         BufferedReader bf;                  //declare the BufferedReader
+         String line;
+         WordList list = new WordList();
+      
+         bf = new BufferedReader(new FileReader("C:/Users/kylet/p2_homework/hw10/words.txt"));    //initialize the buffered reader to word.txt
+      
+         while((line = bf.readLine()) != null)
+         {
+            list.insert(new Word(line));
+         }
+         bf.close();
+         list.display();
+      }
+      catch(IOException e)
+      {
+         System.out.println("Please select a valid file.");
+      }
+      */
+	}
+   
+}
+
---
+++ hw11/WordList.java
@@ -0,0 +1,103 @@
+/**
+ * A linked list of String values.
+ *
+ * @author  Kaden Turner
+ * @version 11/2/2025
+ *
+ */
+public class WordList
+{
+	private WordNode head;
+
+	/**
+	 * A new list has head pointing nowhere.
+	 */
+	public WordList()
+	{
+		head = null;
+	}
+
+	/**
+	 * Displays contents of the list.
+	 */
+	public void display()
+	{
+      WordNode cur = head;
+      while(cur != null)
+      {
+         System.out.print(cur.data + " ");
+         cur = cur.next;
+      }
+	}
+
+	/**
+	 * In an unordered list we can just add to the front.
+	 *
+	 * @param newdata The new element to be inserted into the list.
+	 */
+	public void insert(Word newdata)
+	{  
+      WordNode node = new WordNode();    //create new node
+      node.data = newdata;             //store newdata into new node data  
+      node.next = head;
+      head = node;
+	}
+
+
+
+	/**
+	 * Search the list for the value val.
+	 *
+	 * @param val the value to be searched for
+	 * @return reference to the found node (null if not found)
+	public IntNode search(int val)
+	{
+      IntNode temp = head;
+      while(temp != null)
+      {
+         if(temp.data == val)
+         {
+            System.out.println(temp.data);
+            return temp;
+         }
+         temp = temp.next;
+      }
+      return null;
+	}
+
+	/**
+	 * Find first occurrence of val (if it exists) and remove it from the list.
+	 *
+	 * @param val the value to be removed
+	public void remove(int val)
+	{
+      IntNode q;
+      IntNode p = head;
+      
+      try
+      {
+         if(head.data == val)
+         {
+            head = p.next;    //Condition for if the value is first in list
+         }
+      
+         p = p.next;    //set the p IntNode one ahead of q
+         q = head;      //set the q IntNode one behind of p
+      
+         while(p != null)
+         {
+            if(p.data == val)
+            {
+               q.next = p.next;
+            }
+         p = p.next;
+         q = q.next;
+         }      
+	   }
+      catch(NullPointerException e)    //This will handle the case that head is null
+      {
+         System.out.println("The selected list is empty.");
+      }
+   }
+   */
+}
