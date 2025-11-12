3e33afe81bdeed59cc37767a5c8169bc81d4710d
Kaden Turner <kadenturner19@gmail.com>
2025-11-12 08:18:30 -0600

    Completed pre-lab

---
+++ hw12/ArrayDictionary.java
@@ -0,0 +1,98 @@
+/**
+ * Class that will read from a file, load values into a list and find anagrams from that list
+ *
+ * @author  Kaden Turner
+ * @version 11/5/2025
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ArrayDictionary
+{
+
+   /**
+    * Read words from a file and insert into a list
+    *
+    * @param f file to read from
+    */
+    
+   /* 
+   public ArrayDictionary(String f)
+   {
+      try
+      {
+         BufferedReader numFile;
+         String str;
+         
+         numFile = new BufferedReader(new FileReader(f));
+         while((str = numFile.readLine()) !=null)
+         {
+            insert(new Word(str));
+         }
+         
+         numFile.close();
+      }   
+      catch(IOException e)
+      {
+         System.out.println("Please enter a valid file name.");
+      }
+   
+   }
+   
+   /**
+    * Read through the list and return the count of w's anagrams
+    *
+    * @param w value to compare and find anagrams for
+    
+   public int countAnagrams(Word w)
+   {
+      int count = 0;
+      WordNode c = head;
+      
+      while(c != null)
+      {
+         if(w.anagramCheck(c.data) == true)
+         {
+            count++;
+         }
+         c = c.next;
+      }
+      
+      return count;
+   }
+   
+   /**
+    * Display each word that has more than 6 anagrams
+    
+   public void displayBigAnagramFamilies()
+   {
+      WordNode cur = head;
+      
+      while(cur != null)
+      {
+         WordNode test = head;
+         int count = 0;
+         
+         while(test != null)
+         {
+            if(cur.data.anagramCheck(test.data))
+            {
+               count++;
+            }
+            
+            test = test.next;
+         }
+         if(count >= 6)
+         {
+            System.out.println(cur.data);
+            System.out.println(count);
+         }
+         
+         cur = cur.next;
+      }
+   }
+   */
+}
---
+++ hw12/Driver.java
@@ -0,0 +1,39 @@
+/**
+ * Driver for testing IntList code.
+ *
+ * @author  Kaden Turner
+ * @version 11/5/2025
+ *
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Driver
+{
+	public static void main(String [] args)
+	{
+   
+      Word w1 = new Word("listen");
+      Word w2 = new Word("silent");
+      Word w3 = new Word("kaden");
+      System.out.println(w1.compareTo(w2));
+      System.out.println(w1.compareTo(w3));
+   
+      /*
+      long startTime = System.currentTimeMillis();    //start time
+   
+      LinkedDictionary ld = new LinkedDictionary("C:/Users/kylet/p2_homework/data/mywords.txt");     //declare the linkeddictionary with the test file
+      
+      //System.out.println(ld.countAnagrams(new Word("silent")));         //testing for countAnagrams method
+      
+      ld.displayBigAnagramFamilies();     //testing for displayBigAnagramFamilies method
+      
+      long endTime = System.currentTimeMillis();      //end time
+      
+      double totalTime = (endTime - startTime) / 1000.0;    //format time output
+      System.out.println("Total time: " + totalTime);    //print total time
+      */
+	}  
+}
+
