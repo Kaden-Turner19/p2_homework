9f2770295de2ad9513c6e415591e4843ba4203d7
Kaden Turner <kadenturner19@gmail.com>
2025-11-19 09:14:36 -0600

    Testing for iteration

--- hw13/DemoArrayList.java
+++ hw13/DemoArrayList.java
@@ -6,11 +6,13 @@
  */
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class DemoArrayList
 {
 	public static void main(String [] args)
 	{
+      /*
 		ArrayList<String> al;
 
 		al= new ArrayList<String>();
@@ -21,20 +23,31 @@ public class DemoArrayList
 		System.out.println(al.get(0));
 		System.out.println(al.get(1));
       System.out.println(al.indexOf("hi"));
-      
+      */
       
       ArrayList<Word> wl;
+      ListIterator<Word> it;
       
       wl = new ArrayList<Word>();
       wl.add(new Word("Kaden"));
       wl.add(new Word("Turner"));
       wl.add(new Word("Football"));
+      
+      it = wl.listIterator();     
+      Word temp;
+      
+      while(it.hasNext()) {
+         temp = it.next();
+         System.out.println(temp);
+      }
+      
+      
+      /*
       System.out.println(wl.indexOf(new Word("Kaden")));    //should return 0
       System.out.println(wl.get(1));                        //should return "Turner"
       System.out.println(wl.size());      
       wl.remove(new Word("Football"));
       System.out.println(wl.size());
-      
-      
+      */
 	}
 }
